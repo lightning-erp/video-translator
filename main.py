ca68f60434b59bdb82aaba0e4cbd7f6ff120a440
Artur Stopa <asto@lightning-erp.loc>
2024-01-05 08:21:00 +0100

    alpha version working

--- main.py
+++ main.py
@@ -1,15 +1,4 @@
-import logging
 import os
-
-logging.basicConfig(
-    filename="log.txt",
-    filemode="a",
-    format="%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s",
-    datefmt="%H:%M:%S",
-    level=logging.DEBUG,
-)
-
-logging.info("Begin loading imports")
 
 import tts
 from drive_io import copy_directory_structure, extract_mp3, list_videos
@@ -17,42 +6,26 @@ from subtitles import save_to_srt
 from voice_recognition import transcribe_video
 from voiceover import add_audio_to_video
 
-logging.info("Done loading imports")
-
 IN_DIRECTORY = "videos/in/"
 OUT_DIRECTORY = "videos/out/"
 TEXT_KEYS = ["start", "end", "text"]
 DIAG_KEYS = ["avg_logprob", "no_speech_prob"]
 
 if __name__ == "__main__":
-    logging.info("Begin loop")
     copy_directory_structure(IN_DIRECTORY, OUT_DIRECTORY)
     for filename, extension in [file.split(".") for file in list_videos(IN_DIRECTORY)]:
         file_path = f"{filename}.{extension}"
-        logging.info(f"Begin transcribing {file_path}.{extension}")
         print(f"Transcribing {file_path}.{extension}")
-        logging.debug("Begin extract mp3")
         mp3 = extract_mp3(file_path)
-        logging.debug("End extracting mp3")
-        logging.debug("Begin transcribing video")
         segments = transcribe_video(file_path)
-        logging.debug("End transcriging video")
-        logging.debug("Begin saving transcription to .srt")
-        logging.debug(segments[0])
         save_to_srt(segments, filename.replace(IN_DIRECTORY, OUT_DIRECTORY))
-        logging.debug("End saving transctiption to .srt")
-        logging.debug("Begin generating audio with timestamps")
         MS = 1000
         audios_with_timestamps = [
             (tts.synthesize(segment["text"]), int(segment["start"] * MS))
             for segment in segments
         ]
-        logging.debug("End generating audio with timestamps")
-        logging.debug("Begin adding audio to video")
         add_audio_to_video(
             *zip(*audios_with_timestamps),
             f"{filename}.mp4",
             os.path.join(OUT_DIRECTORY, f"{filename}.{extension}"),
         )
-        logging.debug("End adding audio to video")
-        logging.info(f"Finished transcribing {file_path}")
